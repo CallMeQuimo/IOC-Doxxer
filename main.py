45804ce2d521e2e36c778c8362e5e82752adde70
Quimey <ocampo.quimey@gmail.com>
2025-07-15 15:31:43 -0300

    Mejoras para el ejecutable

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-#from backend.functions import validIOC, opVT, opIPBD
+## Para evitar errores de importación posibles a la hora de compilar
+import sys
+import os
+if getattr(sys, 'frozen', False):
+    os.chdir(sys._MEIPASS)
+
+## Código funcional
 from frontend.gui import launch_gui
 if __name__ == "__main__":
     launch_gui()
